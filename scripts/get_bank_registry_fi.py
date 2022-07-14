26da9f1dbef3ed47fc20d6dd30c2d878a41ce765
pebosi <mail@pebosi.net>
2022-07-14 14:11:12 +0200

    use pandas for processing

--- scripts/get_bank_registry_fi.py
+++ scripts/get_bank_registry_fi.py
@@ -1,31 +1,24 @@
 #!/usr/bin/env python
 import json
+import pandas
 
-import requests
-import xlrd
-
-
-URL = (
-    "https://www.finanssiala.fi/wp-content/uploads/2021/03/"
-    "Finnish_monetary_institution_codes_and_BICs_in_excel_format.xlsx"
-)
+URL = "https://www.finanssiala.fi/wp-content/uploads/2021/03/Finnish_monetary_institution_codes_and_BICs_in_excel_format.xlsx"
 
 
 def process():
-    book = xlrd.open_workbook(file_contents=requests.get(URL).content)
-    sheet = book.sheet_by_index(0)
+    sheet = pandas.read_excel(URL, skiprows=2, sheet_name=0, dtype=str)
 
     return [
         {
             "country_code": "FI",
             "primary": True,
-            "bic": bic.value.upper().strip(),
-            "bank_code": bank_code.value,
-            "name": name.value,
-            "short_name": name.value,
+            "bic": str(bic).upper().strip(),
+            "bank_code": bank_code,
+            "name": name,
+            "short_name": name,
         }
-        for bank_code, bic, name in list(sheet.get_rows())[2:]
-        if bank_code.value != ""
+        for bank_code, bic, name in list(sheet.itertuples(index=False))[2:]
+        if bank_code != ""
     ]
 
 
