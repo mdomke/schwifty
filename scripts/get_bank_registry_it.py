377bd0bb8411df545b171c970f6d9675e85f56bb
Krystof Rehacek <krystofee@gmail.com>
2023-09-01 10:40:32 +0200

    Parsing short names from bank names

--- scripts/get_bank_registry_it.py
+++ scripts/get_bank_registry_it.py
@@ -1,8 +1,89 @@
 import json
+import re
 from time import sleep
 
 import requests
 from bs4 import BeautifulSoup
+
+
+def split_bank_name(s):
+    # The patterns that might suggest the start of the short name.
+    patterns = [
+        r'IN FORMA ABBREVIATA',
+        r'IN BREVE',
+        r'IN SIGLA',
+        r'ABBR\.?',
+        r'O IN FORMA ABBREVIATA',
+        r'OVVERO'
+    ]
+
+    for pattern in patterns:
+        # Special case for the OVVERO pattern
+        if pattern == r'OVVERO':
+            match = re.search(rf"{pattern} ([^\(]*?) O", s)
+        else:
+            match = re.search(rf"{pattern} (.*?)(?=\s*(\(|,|$))", s)
+
+        if match:
+            # Short name is found in the match.
+            short_name = match.group(1).strip().rstrip(')')
+            # Full name is everything before the match.
+            full_name = s[:match.start()]
+            # Further process full_name to remove trailing keywords and extra spaces.
+            for possible_end in ['O', pattern]:
+                full_name = re.sub(rf"\s*{possible_end}\s*$", "", full_name).rstrip(" (")
+            if "OVVERO" in short_name:
+                short_name = short_name.split("OVVERO")[0].strip()
+            return full_name, short_name
+
+    # If no patterns match and the string contains a parenthesis, split at the first parenthesis.
+    if "(" in s:
+        return s.split("(", 1)[0].strip(), None
+
+    # If no patterns match and no parenthesis, just return the original string and None.
+    return s, None
+
+
+def runtime_test_split_bank_name():
+    """
+    Run tests against split_bank_name function. In case there is pattern which is not covered by the function, add it
+    to the patterns list in the function.
+    """
+    test_cases = [
+        ("CASSA DI RISPARMIO DI FERMO S.P.A. (IN FORMA ABBREVIATA CARIFERMO S.P.A.)",
+         ("CASSA DI RISPARMIO DI FERMO S.P.A.", "CARIFERMO S.P.A.")),
+        ("CAIXABANK S.A",
+         ("CAIXABANK S.A", None)),
+        ("IBL ISTITUTO BANCARIO DEL LAVORO S.P.A. (IN FORMA ABBREVIATA IBL BANCA)",
+         ("IBL ISTITUTO BANCARIO DEL LAVORO S.P.A.", "IBL BANCA")),
+        ("BANCA VALSABBINA SOCIETA' COOPERATIVA PER AZIONI (IN BREVE LA VALSABBINA)",
+         ("BANCA VALSABBINA SOCIETA' COOPERATIVA PER AZIONI", "LA VALSABBINA")),
+        ("BANCO DI BRESCIA SAN PAOLO CAB SOCIETA' PER AZIONI (ABBR. BANCO DI BRESCIA S.P.A.)",
+         ("BANCO DI BRESCIA SAN PAOLO CAB SOCIETA' PER AZIONI", "BANCO DI BRESCIA S.P.A.")),
+        ("BANCA DI CIVIDALE SOCIETA' PER AZIONI O IN FORMA ABBREVIATA CIVIBANK S.P.A.",
+         ("BANCA DI CIVIDALE SOCIETA' PER AZIONI", "CIVIBANK S.P.A.")),
+        ("MEDIOCREDITO TRENTINO-ALTO ADIGE - S.P.A. (IN LINGUA TEDESCAINVESTITIONSBANK TRENTINO-SUDTIROL - A.G.)",
+         ("MEDIOCREDITO TRENTINO-ALTO ADIGE - S.P.A.", None)),
+        ("BANCA POPOLARE DELL'ETRURIA E DEL LAZIO - SOCIETA' COOPERATIVA (IN BREVE BANCAETRURIA SOCIETA' COOPERATIVA)",
+         ("BANCA POPOLARE DELL'ETRURIA E DEL LAZIO - SOCIETA' COOPERATIVA", "BANCAETRURIA SOCIETA' COOPERATIVA")),
+        ("CASSA DI RISPARMIO DI ASTI S.P.A. (IN FORMA ABBREVIATA BANCA C.R. ASTI S.P.A.), Filiale di Treviso",
+         ("CASSA DI RISPARMIO DI ASTI S.P.A.", "BANCA C.R. ASTI S.P.A.")),
+        (
+        "BANCA PICCOLO CREDITO VALTELLINESE, SOCIETA' COOPERATIVA (OVVERO CREDITO VALTELLINESE S.C. O SOLO CREDITO VALTELLINESE)",
+        ("BANCA PICCOLO CREDITO VALTELLINESE, SOCIETA' COOPERATIVA", "CREDITO VALTELLINESE S.C.")),
+        (
+            "BANCA DI CREDITO COOPERATIVO - BANCA DI SIRACUSA IN SIGLA BCC BANCA DI SIRACUSA - SOCIETA' COOPERATIVA",
+            ("BANCA DI CREDITO COOPERATIVO - BANCA DI SIRACUSA", "BCC BANCA DI SIRACUSA - SOCIETA' COOPERATIVA")
+        )
+    ]
+
+    for input_str, expected_output in test_cases:
+        # print(f"Testing: {input_str}")
+        # print(f"Expected output: {expected_output}")
+        # print(f"Actual output: {split_bank_name(input_str)}")
+        assert split_bank_name(input_str) == expected_output
+
+    print("test: split_bank_name passed")
 
 
 def get_banks_registry_data_from_bank_name(bank_name):
@@ -28,13 +109,14 @@ def get_banks_registry_data_from_bank_name(bank_name):
             bank_code = row.select("td")[3].text
             bic = row.select("td")[2].text
             if bank_code and bic and row.select("td")[0].text == "IT":
+                bank_name, bank_name_short = split_bank_name(row.select("td")[1].text)
                 yield {
                     'country_code': 'IT',
                     'primary': True,
                     'bic': str(bic).split(',')[0],
                     'bank_code': str(int(bank_code)).zfill(5),
-                    'name': row.select("td")[1].text,
-                    'short_name': row.select("td")[1].text,
+                    'name': bank_name,
+                    'short_name': bank_name_short or bank_name,
                 }
 
 
@@ -61,6 +143,8 @@ def get_italian_bank_names():
 
 
 if __name__ == "__main__":
+    runtime_test_split_bank_name()
+
     bank_names = sorted(set(get_italian_bank_names()))
 
     bic_to_bank = {}
