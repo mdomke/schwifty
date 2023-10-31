421300172e2b2f8696ef2026609446d64c6d8389
sh4dowb <sh4dowb@protonmail.ch>
2023-10-31 17:43:11 +0300

    make lint happy

--- schwifty/checksum/netherlands.py
+++ schwifty/checksum/netherlands.py
@@ -12,12 +12,12 @@ class DefaultAlgorithm(checksum.Algorithm):
     accepts = checksum.InputType.BBAN
 
     def compute(self, bban: str) -> str:
-        raise NotImplementedError('Cannot compute checksum for Netherlands')
+        raise NotImplementedError("Cannot compute checksum for Netherlands")
 
     def validate(self, bban: str) -> bool:
         acc_number = bban[-10:]
         total = 0
         for i, digit in enumerate(map(int, acc_number)):
-            total += digit * (10-i)
+            total += digit * (10 - i)
 
         return total % 11 == 0
