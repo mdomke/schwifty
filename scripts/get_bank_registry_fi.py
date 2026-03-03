e5f947c4a3803be110a13e98e24977343b75fe46
Martin Domke <mail@martindomke.net>
2026-03-03 16:10:52 +0100

    Convert Finish registry to v2-format

--- scripts/get_bank_registry_fi.py
+++ scripts/get_bank_registry_fi.py
@@ -5,6 +5,8 @@ import pandas
 import requests
 from bs4 import BeautifulSoup
 
+from scripts.remap import convert_to_v2
+
 
 BASE_URL = (
     "https://www.finanssiala.fi/en/topics/payment-services-in-finland/payment-technical-documents/"
@@ -23,20 +25,22 @@ def process():
     datas = pandas.read_excel(url, sheet_name=0, dtype=str, header=None)
     datas.fillna("", inplace=True)
 
-    return [
-        {
-            "country_code": "FI",
-            "primary": True,
-            "bic": str(bic).upper().strip(),
-            "bank_code": bank_code,
-            "name": name,
-            "short_name": name,
-        }
-        for bank_code, bic, name in list(datas.itertuples(index=False))[2:]
-        if bank_code != ""
-    ]
+    return convert_to_v2(
+        [
+            {
+                "country_code": "FI",
+                "primary": True,
+                "bic": str(bic).upper().strip(),
+                "bank_code": bank_code,
+                "name": name,
+                "short_name": name,
+            }
+            for bank_code, bic, name in list(datas.itertuples(index=False))[2:]
+            if bank_code != ""
+        ]
+    )
 
 
 if __name__ == "__main__":
-    with open("schwifty/bank_registry/generated_fi.json", "w") as fp:
+    with open("schwifty/bank_registry/generated_fi.v2.json", "w") as fp:
         json.dump(process(), fp, indent=2)
